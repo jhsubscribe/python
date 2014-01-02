99d4765c5b917136345fc21dad2216d494e376b3
jhsubscribe <jhsubscribe@bigpond.com>
2014-01-02 17:26:09 +0000

    altinstall as default
    avoiding merge issues

--- attributes/default.rb
+++ attributes/default.rb
@@ -42,7 +42,8 @@ default['python']['url'] = 'http://www.python.org/ftp/python'
 default['python']['version'] = '2.7.5'
 default['python']['checksum'] = '3b477554864e616a041ee4d7cef9849751770bc7c39adaf78a94ea145c488059'
 default['python']['configure_options'] = %W{--prefix=#{python['prefix_dir']}}
-default['python']['make_options'] = %W{install}
+# changing default to altinstall (as merging otherwise)
+default['python']['make_options'] = %W{altinstall}
 
 default['python']['pip_location'] = "#{node['python']['prefix_dir']}/bin/pip"
 default['python']['virtualenv_location'] = "#{node['python']['prefix_dir']}/bin/virtualenv"
